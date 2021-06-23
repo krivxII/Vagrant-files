727934639649d1e7447bdd59c9919a129e5be242
Herman Anez <herman.anez@alumnos.uneatlantico.es>
2021-06-23 13:35:38 +0200

    caja de la tesis

---
+++ ubuntu-focal64  20.4/TFG/bootstrap.sh
@@ -0,0 +1,26 @@
+# Update Packages
+sudo apt-get update
+# Upgrade Packages
+sudo apt-get upgrade
+sudo apt-get dist-upgrade
+
+# Basic Linux Stuff
+sudo apt-get install -y git
+
+sudo apt-get install -y redis-server
+
+sudo apt install npm -y
+
+sudo npm cache clean -f
+
+sudo npm install -g n
+
+sudo n 14.17.1
+
+sudo apt upgrade -y
+
+# sudo apt install curl
+
+# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.38.0/install.sh | bash
+
+
