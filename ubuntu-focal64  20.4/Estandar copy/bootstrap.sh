ee95e87161b84955a905edd6ab624e76b6ae5b53
Herman Anez <herman.anez@alumnos.uneatlantico.es>
2021-07-16 16:30:03 +0200

    -

--- ubuntu-focal64  20.4/Estandar copy/bootstrap.sh
+++ ubuntu-focal64  20.4/Estandar copy/bootstrap.sh
@@ -5,20 +5,16 @@ sudo apt-get upgrade
 sudo apt-get dist-upgrade
 
 # Basic Linux Stuff
-
-# install git
 sudo apt-get install -y git
 
-#install redis-server
-sudo apt-get install -y redis-server
-
-# install npm
 sudo apt install npm -y
 
-# Install node trugh npm
 sudo npm cache clean -f
+
 sudo npm install -g n
+
 sudo n 14.17.1
+
 sudo apt upgrade -y
 
 # sudo apt install curl
